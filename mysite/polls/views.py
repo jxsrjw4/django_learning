07745db10729505d5e95025a2eaa0fc6ae567881
wen.jiang <wen.jiang@jinkosolar.com>
2019-10-29 21:12:55 +0800

    add template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,15 @@
 from django.http import  HttpResponse
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse("hello world!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
