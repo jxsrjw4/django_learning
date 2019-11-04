8427b3518eca69899fa10355f5f19a7abb89ad5a
wen.jiang <wen.jiang@jinkosolar.com>
2019-11-04 20:35:58 +0800

    上传polls应用相关的视图

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
-from django.http import  HttpResponse
-from .models import Question
+from django.http import  HttpResponse,Http404,HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,10 +14,24 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    try:
+        question  = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,"polls/detail.html", {'question':question})
 
 def results(request, question_id):
-    response="You'are looing at the result of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,"polls/result.html", {'question':question})
+
+
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html', {'question':question, 'error_msg':"You didn't select a choice."})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
