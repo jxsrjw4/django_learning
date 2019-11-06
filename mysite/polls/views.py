233be70298289ae2e9d64b590339945faf026e33
wen.jiang <wen.jiang@jinkosolar.com>
2019-11-06 20:12:58 +0800

    '使用通用视图'

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,24 @@ from .models import Question,Choice
 from django.template import loader
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result'
+
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
